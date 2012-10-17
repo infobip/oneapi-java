992d9d831c9a6220f48d446a97ca1cea9350b722
Vatroslav Vukovic <vatroslav.vukovic@gmail.com>
2012-10-17 11:38:11 +0200

    Moved ussd examples to new package

--- src/test/java/oneapi/examples/ussd/SendUSSDAndStopSessionExample.java
+++ src/test/java/oneapi/examples/ussd/SendUSSDAndStopSessionExample.java
@@ -1,15 +1,15 @@
-package oneapi.examples.smsmessaging;
+package oneapi.examples.ussd;
 
 import oneapi.client.impl.SMSClient;
 import oneapi.config.Configuration;
 import oneapi.model.USSDRequest;
 
-public class SendUSSDExample {
+public class SendUSSDAndStopSessionExample {
 
 	public static void main(String[] args) throws Exception {
 		Configuration configuration = new Configuration("user1", "user_password1");
 		SMSClient smsClient = new SMSClient(configuration);
 
-		smsClient.getUSSDClient().sendUSSD(new USSDRequest("11111111111111111111111111", "Hi"));
+		smsClient.getUSSDClient().sendUSSD(new USSDRequest("11111111111111111111111111", "Hi", true));
 	}
 }
