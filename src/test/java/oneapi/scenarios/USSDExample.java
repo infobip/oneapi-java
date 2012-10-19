b7cbf79858ab1ce63502619489d8bfae5a65577e
Vatroslav Vukovic <vatroslav.vukovic@gmail.com>
2012-10-19 14:22:10 +0200

    Added USSDExample.java to Scenarios

---
+++ src/test/java/oneapi/scenarios/USSDExample.java
@@ -0,0 +1,57 @@
+package oneapi.scenarios;
+
+import org.apache.log4j.BasicConfigurator;
+
+import oneapi.PropertyLoader;
+import oneapi.client.impl.SMSClient;
+import oneapi.config.Configuration;
+import oneapi.model.common.InboundSMSMessage;
+
+
+/**
+ * To run this example follow these 3 steps:
+ *
+ *  1.) Download 'OneApi Java library' - available at github.com/parseco
+ *
+ *  2.) Open 'scenarios.USSDExample' class to edit where you should populate the following fields: 
+ *		'DESTINATION'    	'MESSAGE'
+ *		'USERNAME'                     
+ *		'PASSWORD'           
+ *
+ *  3.) Run the example class by right click it and select 'Run As -> Java Application' 
+ **/
+
+public class USSDExample {
+
+	// ----------------------------------------------------------------------------------------------------
+	// TODO: Fill you own values here or create/change the example.properties file:
+	// ----------------------------------------------------------------------------------------------------
+
+	private static final String USERNAME = PropertyLoader.loadProperty("example.properties", "username");
+	private static final String PASSWORD = PropertyLoader.loadProperty("example.properties", "password");
+	private static final String DESTINATION = PropertyLoader.loadProperty("example.properties", "destination");
+	private static final String MESSAGE = "You language of choice?\n1. Java\n2. .NET"; 
+	
+	public static void main(String[] args) {
+		
+		// Configure logger
+		BasicConfigurator.configure();
+		
+
+		// Initialize Configuration object 
+		Configuration configuration = new Configuration(USERNAME, PASSWORD);
+
+		// Initialize SMSClient using the Configuration object
+		SMSClient smsClient = new SMSClient(configuration);
+
+		String response = null;
+		while(response == null || !"1".equals(response)) {
+			//Send USSD and wait for the answer
+			InboundSMSMessage inboundMessage = smsClient.getUSSDClient().sendMessage(DESTINATION, MESSAGE);
+			response = inboundMessage.getMessage();
+		}
+
+		// Send message and stop USSD session
+		smsClient.getUSSDClient().stopSession(DESTINATION, "Cool");
+	}
+}
